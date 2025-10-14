512b8d2ab4bf46e36a8e2f6de65288e6f62fcecf
palomalevy <palomalevy@gmail.com>
2025-10-13 23:51:18 -0400

    Initial backend + frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>AutoMatch</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
